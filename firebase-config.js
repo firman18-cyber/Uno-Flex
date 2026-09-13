fe5f2cc1fe5bd0da70b8b2c4b7e6d11a717498be
firman18-cyber <67278774+firman18-cyber@users.noreply.github.com>
2026-09-14 06:23:45 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,10 @@
-export const firebaseConfig={apiKey:"ISI_API_KEY",authDomain:"ISI_PROJECT.firebaseapp.com",databaseURL:"https://ISI_DATABASE.firebasedatabase.app",projectId:"ISI_PROJECT",storageBucket:"ISI_PROJECT.firebasestorage.app",messagingSenderId:"ISI_SENDER_ID",appId:"ISI_APP_ID"};
+// Firebase Console > Project settings > Your apps > Web app
+export const firebaseConfig = {
+  apiKey: "AIzaSyAsUmrDn3pI8J4q1XnTU4KcnFeUgHgByZY",
+  authDomain: "uno-flex-f36e8.firebaseapp.com",
+  databaseURL: "https://uno-flex-f36e8-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "uno-flex-f36e8",
+  storageBucket: "uno-flex-f36e8.firebasestorage.app",
+  messagingSenderId: "878978021695",
+  appId: "1:878978021695:web:3995ae12c7575016497b8a"
+};
