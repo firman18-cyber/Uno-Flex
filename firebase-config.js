5ee71c28137b6886a0720ff3b513e1a36903547e
firman18-cyber <67278774+firman18-cyber@users.noreply.github.com>
2026-09-14 22:25:28 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebas
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAsUmrDn3pI8J4q1XnTU4KcnFeUgHgByZY",
-    authDomain: "uno-flex-f36e8.firebaseapp.com",
-    databaseURL: "https://uno-flex-f36e8-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "uno-flex-f36e8",
-    storageBucket: "uno-flex-f36e8.firebasestorage.app",
-    messagingSenderId: "878978021695",
-    appId: "1:878978021695:web:3995ae12c7575016497b8a"
+    apiKey: "AIzaSyA4iB_gL7aZfw_mo0jqiy9LZSgzhCMdFfM",
+    authDomain: "project-1a1b8162-b905-4fc0-b2b.firebaseapp.com",
+    databaseURL: "https://project-1a1b8162-b905-4fc0-b2b-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "project-1a1b8162-b905-4fc0-b2b",
+    storageBucket: "project-1a1b8162-b905-4fc0-b2b.firebasestorage.app",
+    messagingSenderId: "919755455649",
+    appId: "1:919755455649:web:bffb68dd64f2493f1f84e0"
 };
 
 const app = initializeApp(firebaseConfig);
